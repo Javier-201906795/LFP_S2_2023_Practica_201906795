f513126daf3b9ac7f702c38474c75693b5794328
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-08-18 17:10:06 -0600

    pruebas con diccionarios

--- diccionario.py
+++ diccionario.py
@@ -10,8 +10,34 @@ print("Hola mundo")
 productos = {}
 
 
-productos['manzana']='comida'
+productos['manzana']={'cantidad':100,'precio':2.5,'ubicacion':'bodeaga-A'}
+productos['pera']={'cantidad':80,'precio':5.0,'ubicacion':'bodeaga-B'}
 
 
 
 print(productos)
+print('------')
+print(productos['manzana'])
+print('------')
+print(productos['manzana']['cantidad'])
+print('------')
+
+print('### Buscando Peras ###')
+if 'pera' in productos:
+    print("hay peras")
+else:
+    print("no hay peras")
+
+
+print('### Buscando Uvas ###')
+if 'uva' in productos:
+    print("hay uvas")
+else:
+    print("no hay uvas")
+
+
+print('### Buscando Peras en bodega-B ###')
+if 'bodeaga-B' in productos['pera']['ubicacion']:
+    print("SI hay peras en bodega-b")
+else:
+    print("no hay peras en bodega-b")
