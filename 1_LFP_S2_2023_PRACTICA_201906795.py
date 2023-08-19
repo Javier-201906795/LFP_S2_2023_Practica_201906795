5637e2f8ad53f16aedc996dc2eb24ea86d10944e
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-08-19 11:55:23 -0600

    opcion 1 abrir archivo .inv
    pide al usario que ingrese la localizacion y nombre de un archivo .inv para luego abrirlo leerlo y crear un listado separado por los saltos de linea

--- 1_LFP_S2_2023_PRACTICA_201906795.py
+++ 1_LFP_S2_2023_PRACTICA_201906795.py
@@ -1,4 +1,14 @@
 
+import os
+
+
+#Variables globales
+archivo = {'extension': None, 'validador': False, 'data': None}
+
+#Inicializar variables globales
+archivo['extension']= None
+archivo['validador']= False
+archivo['data']= None
 
 
 
@@ -35,18 +45,91 @@ def menu():
     return opcionvalida
 
 
-def opcion1():
-   print('opcion 1')
+def abrirarchivo(extensionvalida):
+    #Mensaje
+    print('\nSeleccione un archivo'+ str(extensionvalida)+'\n')
+    print('-------------------------------------------------------------------')
+    print('Nota: el archivo debe estar en la misma carpeta que el proyecto.')
+    print('Nota 2: si desea salir presione el numero " 4 ".')
+    print('-------------------------------------------------------------------')
+    
+    ruta = input("Ingrese la ruta del archivo: ")
 
+    while True:
+        print('ruta', ruta)
+        #Salir 
+        if ruta == '4':
+           break
+        #Validar extension
+        nombre, extension = os.path.splitext(ruta)
+        print('extension',extension)
+        if extension == str(extensionvalida):
+            print("\nArchivo valido\n")
+            break
+        else:
+            print("\nArchivo invalido\n")
+            ruta = input("\nIngrese la ruta del archivo: ")
+
+    
+    #Abrir archivo  
+    listaDatos = []
+    try:
+      with open(ruta, "r") as archivo:
+        for linea in archivo:
+            #Array por salto de linea
+            array = linea.split('\n')
+            for i in range(len(array)):
+                if array[i] == '' or array[i] == ' ':
+                  None
+                else:
+                  listaDatos.append(array[i])
+    except:
+      print("Error: Al abrir el archivo.")
+    
+    print(listaDatos) 
+
+    
+
+#/////////////////////////////////////////////////
+def opcion1():
+  print('# Cargar inventario inicial:')
+  abrirarchivo('.inv')
+  # listaDatos = []
+  # print("\nHa seleccionado cargar archivo .inv\n")
+  # ruta = input("Ingrese la ruta del archivo: ")
+
+  # while True:
+  #     nombre, extension = os.path.splitext(ruta)
+  #     if extension == ".inv":
+  #         print("\nArchivo valido\n")
+  #         break
+  #     else:
+  #         print("\nArchivo invalido\n")
+  #         ruta = input("\nIngrese la ruta del archivo: ")
+
+  # with open(ruta, "r") as archivo:
+  #         for linea in archivo:
+  #             array = linea.split(',')
+  #             for i in range(len(array)):
+  #                 listaDatos.append(array[i])
+  
+  # print(listaDatos)
+
+
+
+
+#/////////////////////////////////////////////////
 def opcion2():
    print('opcion 2')
 
+#/////////////////////////////////////////////////
 def opcion3():
    print('opcion3')
 
+#/////////////////////////////////////////////////
 def opcion4():
    print('opcion4')
-
+#/////////////////////////////////////////////////
 
 
 
@@ -55,8 +138,6 @@ def opcion4():
 print(mensajebienvenida()) 
 
 opcionmenu = menu()
-
-print('opcion seleccionada: ', opcionmenu)
 
 #SWITCH
 if opcionmenu == 1:
@@ -67,3 +148,6 @@ elif opcionmenu == 3:
    opcion3()
 elif opcionmenu == 4:
    opcion4()
+
+
+################################################################
