d5acf7563dec9735edb2fda7f5a162412639c681
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-08-18 18:47:19 -0600

    menu

--- 1_LFP_S2_2023_PRACTICA_201906795.py
+++ 1_LFP_S2_2023_PRACTICA_201906795.py
@@ -13,10 +13,12 @@ def mensajebienvenida():
 
 
 def menu():
-    mensaje =  '# Sistema de inventario:\n'
+    mensaje =  '# Sistema de inventario:\n\n'
     mensaje += '1) Cargar inventario inicial\n'
     mensaje += '2) Cargar instrucciones movimientos\n'
     mensaje += '3) Crear informe de inventario\n'
+    mensaje += '4) Salir\n'
+    mensaje += '\n'
     return mensaje
 
 
@@ -29,3 +31,17 @@ def menu():
 ################################################################
 
 print(mensajebienvenida()) 
+print(menu())
+#Seleccione una opcion
+opcion = input()
+print(opcion)
+#Validar opcion
+try:
+  opcion = int(opcion)
+except:
+  print("Ingrese un opcion numerica, porfavor vuelva a intentar.")
+
+
+if opcion == 1:
+    print('hola')
+
